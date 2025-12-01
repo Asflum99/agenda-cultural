0120cdcb17069f05bd4f4e3d1df49ef721b65fa8
Asflum99 <asflum99@gmail.com>
2025-12-01 13:03:27 -0500

    Migrate from timezone aware to timezone naive

--- agenda_cultural/backend/services/database_service.py
+++ agenda_cultural/backend/services/database_service.py
@@ -1,14 +1,15 @@
 import reflex as rx
 from datetime import datetime
 from zoneinfo import ZoneInfo
-from agenda_cultural.backend.models import Movie
+from agenda_cultural.backend.models import Movies
 
 
 def cleanup_past_movies():
     """Elimina películas ya proyectadas de la base de datos"""
     with rx.session() as session:
-        today = datetime.now(ZoneInfo("America/Lima"))
-        movies = session.exec(Movie.select().where(Movie.date < today)).all()
+        today_aware = datetime.now(ZoneInfo("America/Lima"))
+        today_naive = today_aware.replace(tzinfo=None)
+        movies = session.exec(Movies.select().where(Movies.date < today_naive)).all() # pyright: ignore
         for movie in movies:
             session.delete(movie)
         session.commit()
@@ -17,18 +18,23 @@ def cleanup_past_movies():
 def has_upcoming_movies() -> bool:
     """Verifica si hay películas por proyectarse en la BD"""
     with rx.session() as session:
-        now = datetime.now(ZoneInfo("America/Lima"))
-        query = Movie.select().where(Movie.date > now).limit(1)
+        now_aware = datetime.now(ZoneInfo("America/Lima"))
+        now_naive = now_aware.replace(tzinfo=None)
+        query = (
+            Movies.select()
+            .where(Movies.date is not None, Movies.date > now_naive) # pyright: ignore
+            .limit(1)
+        )
         future_movie = session.exec(query).first()
         return future_movie is not None
 
 
-def save_movies_to_db(movies: list[Movie]):
+def save_movies_to_db(movies: list[Movies]):
     """Guarda lista de películas en la BD"""
     try:
         with rx.session() as session:
             db_movies = [
-                Movie(
+                Movies(
                     title=movie.title,
                     location=movie.location,
                     date=movie.date,
