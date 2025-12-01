332e8a5ae7173fae5328c2d795121ca5b01df377
Asflum99 <asflum99@gmail.com>
2025-12-01 13:04:21 -0500

    Decouple main file

--- agenda_cultural/agenda_cultural.py
+++ agenda_cultural/agenda_cultural.py
@@ -1,19 +1,7 @@
-# pyright: reportUnusedImport=false, reportUnknownVariableType=false
-
 import reflex as rx
-import asyncio
 
 from rxconfig import config
 from agenda_cultural.frontend.pages import home
-from agenda_cultural.backend.app_initializer import initialize_app
-from agenda_cultural.backend.models import Movie
-
-rx.Model.create_all()
-
-asyncio.run(initialize_app())
+from agenda_cultural.backend.models import Movies
 
 app = rx.App()
-
-
-async def main():
-    await initialize_app()
