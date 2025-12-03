c4d05ee17e8adff61e1cbabbbb01480ee4bc8edc
Asflum99 <asflum99@gmail.com>
2025-12-03 15:35:35 -0500

    Add DNS

--- rxconfig.py
+++ rxconfig.py
@@ -1,16 +1,25 @@
 import reflex as rx
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
+if os.getenv("REFLEX_ENV") == "prod":
+    # CONFIGURACIÓN PARA AWS
+    my_api_url = "https://agendacultural.duckdns.org"
+else:
+    # CONFIGURACIÓN PARA LOCAL
+    my_api_url = "http://localhost:8000"
+
 config = rx.Config(
     app_name="agenda_cultural",
+    # Asignamos la URL dinámica
+    api_url=my_api_url,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ],
+    # Base de Datos
     db_url=os.getenv("DATABASE_URL"),
 )
