3f8e6d5c34d40ad06def436c8379c6c32c1ab911
Asflum99 <asflum99@gmail.com>
2025-12-10 17:10:51 -0500

    fix logger error

--- agenda_cultural/backend/__init__.py
+++ agenda_cultural/backend/__init__.py
@@ -1,4 +1,5 @@
 from .models import Movies
 from .log_config import get_task_logger
+from .app_initializer import initialize_app
 
-__all__ = ["Movies", "get_task_logger"]
+__all__ = ["Movies", "get_task_logger", "initialize_app"]
