71d4c72ee0b4173cca0b98008d3c901df4e165aa
Asflum99 <asflum99@gmail.com>
2026-01-21 10:25:33 -0500

    refactor: standarize 'alianza_francesa' key to snake_case

--- agenda_cultural/shared/cultural_centers.py
+++ agenda_cultural/shared/cultural_centers.py
@@ -1,9 +1,6 @@
 CULTURAL_CENTERS = {
-    "alianza francesa": {"name": "Alianza Francesa", "short_name": "Alianza"},
-    "bnp": {"name": "Biblioteca Nacional del Perú", "short_name": "BNP"},
-    "ccpucp": {"name": "Centro Cultural PUCP", "short_name": "CCPUCP"},
-    "lum": {
-        "name": "Lugar de la Memoria, la Tolerancia y la Inclusión Social",
-        "short_name": "LUM",
-    },
+    "alianza_francesa": {"name": "Alianza Francesa"},
+    "bnp": {"name": "Biblioteca Nacional del Perú"},
+    "ccpucp": {"name": "Centro Cultural PUCP"},
+    "lum": {"name": "Lugar de la Memoria, la Tolerancia y la Inclusión Social"},
 }
